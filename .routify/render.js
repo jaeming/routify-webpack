76dfa30e264df8451a35dedeeb126eff4990e4b0
Benji <benjid@knowbe4.com>
2022-08-23 14:32:47 -0400

    remove stuff

--- .routify/render.js
+++
@@ -1,5 +0,0 @@
-
-        import * as module from '../src/App.svelte'
-        import { renderModule } from '@roxi/routify/tools'
-
-        export const render = url => renderModule(module, url)
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # routify 3 webpack
-
-See webpack.config.js for notes on issues.
